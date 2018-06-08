a4144b3eb609063612e32693d9f96f6dffdc363c
Hikaru Suido <hikaru.suido@gmail.com>
2018-06-09 08:25:54 +0900

    Update

--- UdpReceive.java
+++ UdpReceive.java
@@ -1,22 +1,26 @@
 import java.net.*;
 import java.io.*;
 
-public class UdpReceive{
-    UdpReceive(){
-	byte buffer[] = new byte[40];
-	try{
-	    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-	    DatagramSocket dsocket = new DatagramSocket(4321);
-	    while(true){
-		dsocket.receive(packet);
-		String msg = new String(buffer, 0, packet.getLength());
-		System.out.println(packet.getAddress().getHostName() + ": " + msg);
-	    }
-	}catch(IOException e){
-	    System.err.println(""+e);
+public class UdpReceive {
+	UdpReceive() {
+		byte buffer[] = new byte[40];
+		try {
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			DatagramSocket dsocket = new DatagramSocket(4321);
+			while (true) {
+				dsocket.receive(packet);
+				String name = new String(buffer, 0, 8);
+				String message = new String(buffer, 8, 32);
+
+				System.out.println("From :" + name);
+				System.out.println(message);
+			}
+		} catch (IOException e) {
+			System.err.println("" + e);
+		}
 	}
-    }
-    public static void main(String args[]){
-	new UdpReceive();
-    }
+
+	public static void main(String args[]) {
+		new UdpReceive();
+	}
 }
--- UdpSend.java
+++ UdpSend.java
@@ -1,38 +1,37 @@
 import java.net.*;
 import java.io.*;
-import java.util.Arrays;
 
-public class UdpSend{
-    
-    UdpSend(String args[]){
-	if(args.length!=1){
-	    System.exit(1);
+public class UdpSend {
+
+    UdpSend(String args[]) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+
+        String firstString = "tmp";
+        String secondString = "test";
+
+        byte[] first = new byte[8];
+        first = firstString.getBytes();
+        byte[] second = new byte[32];
+        second = secondString.getBytes();
+        byte[] destination = new byte[40];
+
+        System.arraycopy(first, 0, destination, 0, first.length);
+        System.arraycopy(second, 0, destination, 8, second.length);
+
+        try {
+            InetAddress address = InetAddress.getByName(args[0]);
+            DatagramPacket packet = new DatagramPacket(destination, destination.length, address, 4321);
+            DatagramSocket s = new DatagramSocket();
+            s.send(packet);
+            s.close();
+        } catch (IOException e) {
+            System.err.println("" + e);
+        }
     }
 
-    String firstString = "tmp";
-    String secondString = "test";
-
-    byte[] first  = new byte[8];
-    first = firstString.getBytes();
-    byte[] second = new byte[32];
-    second = secondString.getBytes();
-    byte[] destination = new byte[40];
-
-    System.arraycopy(first, 0, destination, 0, first.length);
-    System.arraycopy(second, 0, destination, 8, second.length);
-
-	try{
-	    InetAddress address = InetAddress.getByName(args[0]);
-	    DatagramPacket packet = new DatagramPacket(destination, destination.length, address, 4321);
-	    DatagramSocket s = new DatagramSocket();
-	    s.send(packet);
-	    s.close();
-	}catch(IOException e){
-	    System.err.println(""+e);
-	}
-    }
-    
-    public static void main(String args[]){
-	new UdpSend(args);
+    public static void main(String args[]) {
+        new UdpSend(args);
     }
 }
